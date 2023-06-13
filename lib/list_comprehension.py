218d7f01bc3447d8894690f163522962a1e8d4d2
Louis Klein <louisklein71@gmail.com>
2023-06-13 13:18:17 -0700

    test

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+even_list = []
 
 def return_evens(num_list):
-    pass
+    for num in num_list:
+        if num %2 == 0:
+            even_list.append(num)
+    return even_list
 
+exclamation_list = []
 def make_exclamation(sentence_list):
-    pass
+    for sentence in sentence_list:
+        exclamation_list.append(sentence + "!")
+    return exclamation_list
